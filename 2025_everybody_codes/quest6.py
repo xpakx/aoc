9e871788b9952fd97a9e13b0a6810355f56553d9
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-01 22:41:41 +0100

    feat(ec2025): quest 6 part 3

--- 2025_everybody_codes/quest6.py
+++ 2025_everybody_codes/quest6.py
@@ -30,5 +30,56 @@ def task2(data):
     return result
 
 
+def print_bin(num):
+    print(''.join(reversed(format(num, '#030b')[2:])))
+
+
+# doesn't work on example bc len(data) < dist
+# prolly sliding window would be a better approach
+def task3(data):
+    dist = 1000
+    rep = 1000
+    ln = len(data)*rep
+    print(ln)
+    letter_masks = defaultdict(int)
+    for i in range(len(data)):
+        c = data[i % len(data)]
+        if c.isupper():
+            m = letter_masks[c.lower()]
+            m ^= 1 << i
+            letter_masks[c.lower()] = m
+
+    result = 0
+    result_prev = 0
+    result_next = 0
+    mask = (1 << dist+1) - 1
+    prev_mask = ((1 << dist) - 1) << (len(data) - dist)
+    next_mask = 0
+    for i in range(len(data)):
+        c = data[i % len(data)]
+        if c.islower():
+            m = letter_masks[c]
+            r = m & mask
+            result += r.bit_count()
+            r = m & prev_mask
+            result_prev += r.bit_count()
+            r = m & next_mask
+            result_next += r.bit_count()
+        mask <<= 1
+        prev_mask <<= 1
+        next_mask <<= 1
+        if i < dist:
+            mask ^= 1
+        if i >= len(data) - dist - 1:
+            next_mask ^= 1
+
+    if rep == 1:
+        return result
+    first_pass = result + result_next
+    last_pass = result_prev + result
+    other_pass = result_prev + result + result_next
+    return first_pass + last_pass + max(0, rep-2)*other_pass
+
+
 app = AdventDay()
 app.run()
