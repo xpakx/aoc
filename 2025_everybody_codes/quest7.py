c97bd1148bdefbc6ce9e76993bf062b4a27b1dbd
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-02 18:34:11 +0100

    feat(ec2025): quest 7 part 3

--- 2025_everybody_codes/quest7.py
+++ 2025_everybody_codes/quest7.py
@@ -55,5 +55,45 @@ def task2(names, rules):
     return result
 
 
+def task3(names, rules):
+    r = {}
+    for rule in rules:
+        r[rule.first] = rule.to
+
+    options = set()
+    visited = set()
+    for name in sorted(names, key=len):
+        correct = True
+        for i in range(len(name)-1):
+            curr_letter = name[i]
+            next_letter = name[i+1]
+            candidates = r[curr_letter]
+            if not candidates or next_letter not in candidates:
+                correct = False
+                break
+        if not correct:
+            continue
+        letter = name[-1]
+        candidates = [name + x for x in r[letter]]
+        print(name)
+
+        while len(candidates) != 0:
+            suffix = candidates.pop()
+            if suffix in visited:
+                continue
+            visited.add(suffix)
+            if len(suffix) > 11:
+                continue
+            if len(suffix) >= 7:
+                options.add(suffix)
+            letter = suffix[-1]
+            if letter not in r:
+                continue
+            new_letters = [suffix + x for x in r[letter]]
+            candidates.extend(new_letters)
+
+    return len(options)
+
+
 app = AdventDay()
 app.run()
