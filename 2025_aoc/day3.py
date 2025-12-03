641e32de240961f3edc1781b6d2f411a852f387b
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-03 15:46:55 +0100

    feat(aoc2025): day 3 part 2

--- 2025_aoc/day3.py
+++ 2025_aoc/day3.py
@@ -23,5 +23,24 @@ def task1(data):
     return result
 
 
+def task2(data):
+    result = 0
+    for rating in data:
+        number = [0]*12
+        for i in range(len(rating)):
+            c = rating[i]
+            num = ord(c) - 48
+            for j in range(len(number)):
+                n = number[j]
+                if num > n and i < len(rating) - (12-j) + 1:
+                    number[j] = num
+                    for k in range(j+1, len(number)):
+                        number[k] = 0
+                    break
+        for i, n in enumerate(reversed(number)):
+            result += n*10**i
+    return result
+
+
 app = AdventDay()
 app.run()
