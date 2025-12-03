e8fb9f9d8b1b424a056821af4523aa220fb61094
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-03 16:09:15 +0100

    refactor(aoc2025): extract calculation for day 3

--- 2025_aoc/day3.py
+++ 2025_aoc/day3.py
@@ -6,39 +6,35 @@ def load(filename):
     return get_file(filename)
 
 
+def get_batteries(rating, batteries):
+    number = [0]*batteries
+    for i in range(len(rating)):
+        c = rating[i]
+        num = ord(c) - 48
+        for j in range(len(number)):
+            n = number[j]
+            if num > n and i < len(rating) - (batteries-j) + 1:
+                number[j] = num
+                for k in range(j+1, len(number)):
+                    number[k] = 0
+                break
+    result = 0
+    for i, n in enumerate(reversed(number)):
+        result += n*10**i
+    return result
+
+
 def task1(data):
     result = 0
     for rating in data:
-        max = 0
-        next = 0
-        for i in range(len(rating)):
-            c = rating[i]
-            num = ord(c) - 48
-            if num > max and i < len(rating)-1:
-                max = num
-                next = 0
-            elif num > next:
-                next = num
-        result += max*10 + next
+        result += get_batteries(rating, 2)
     return result
 
 
 def task2(data):
     result = 0
     for rating in data:
-        number = [0]*12
-        for i in range(len(rating)):
-            c = rating[i]
-            num = ord(c) - 48
-            for j in range(len(number)):
-                n = number[j]
-                if num > n and i < len(rating) - (12-j) + 1:
-                    number[j] = num
-                    for k in range(j+1, len(number)):
-                        number[k] = 0
-                    break
-        for i, n in enumerate(reversed(number)):
-            result += n*10**i
+        result += get_batteries(rating, 12)
     return result
 
 
