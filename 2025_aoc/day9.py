53f15eb846b94d12330b177ae69ef09de78d8f8d
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-09 17:51:41 +0100

    feat(aoc2025): compress points in day 9

--- 2025_aoc/day9.py
+++ 2025_aoc/day9.py
@@ -2,7 +2,7 @@ from utils.loader import get_file
 from utils.parser import parse
 from utils.runner import AdventDay
 from dataclasses import dataclass
-from itertools import combinations
+from itertools import combinations, groupby
 
 
 @dataclass
@@ -29,5 +29,75 @@ def task1(data):
     return max
 
 
+def to_sorted_grid(points):
+    points = sorted(points, key=lambda p: (p.y, p.x))
+    result = []
+    for _, group in groupby(points, key=lambda p: p.y):
+        result.append(list(group))
+    return result
+
+
+@dataclass
+class CompressedPoint:
+    x: int
+    y: int
+    true_x: int
+    true_y: int
+
+    @staticmethod
+    def of_point(p, x, y):
+        return CompressedPoint(x=x, y=y, true_x=p.x, true_y=p.y)
+
+
+def compress(points):
+    all_x = set()
+    for row in points:
+        for p in row:
+            all_x.add(p.x)
+    sorted_unique_x = sorted(list(all_x))
+    x_map = {val: i for i, val in enumerate(sorted_unique_x)}
+
+    compressed = []
+    y = 0
+    for row in points:
+        r = []
+        for p in row:
+            x = x_map[p.x]
+            r.append(CompressedPoint.of_point(p, x, y))
+        compressed.append(r)
+        y += 1
+    print(compressed)
+    return compressed
+
+def print_map(points):
+    max_x = 0
+    max_y = 0
+    for row in points:
+        for p in row:
+            if p.x > max_x:
+                max_x = p.x
+            if p.y > max_y:
+                max_y = p.y
+    for y in range(max_y+1):
+        for x in range(max_x+1):
+            point = False
+            for row in points:
+                for p in row:
+                    if (x == p.x and y == p.y):
+                        print("#", end="")
+                        point = True
+                        break
+            if point:
+                continue
+            print(".", end="")
+        print()
+
+
+def task2(data):
+    points = to_sorted_grid(data)
+    compressed = compress(points)
+    print_map(compressed)
+
+
 app = AdventDay()
-app.run(test=False)
+app.run(test=True)
