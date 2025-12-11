5023130a0e7254afddd45ee825eed4a206a857bc
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-11 13:50:49 +0100

    feat(aoc2025): day 11 part 2

--- 2025_aoc/day11.py
+++ 2025_aoc/day11.py
@@ -32,7 +32,40 @@ def all_paths(start: str, end: str, conns) -> int:
 
 def task1(data):
     conns = {conn.id: conn for conn in data}
-    return all_paths("you", "out", conns)
+    # return all_paths("you", "out", conns)
+    return all_paths_recur("you", "out", conns, {})
+
+
+def all_paths_recur(current: str, end: str, conns, visited) -> int:
+    if current in visited:
+        return visited[current]
+
+    if current == end:
+        return 1
+
+    paths = 0
+    conn = conns.get(current)
+    if not conn:
+        return 0
+    for c in conn.nodes:
+        paths += all_paths_recur(c, end, conns, visited)
+
+    visited[current] = paths
+    return paths
+
+
+def task2(data):
+    conns = {conn.id: conn for conn in data}
+    svr_to_dac = all_paths_recur('svr', 'dac', conns, {})
+    dac_to_fft = all_paths_recur('dac', 'fft', conns, {})
+    fft_to_out = all_paths_recur('fft', 'out', conns, {})
+    option1 = svr_to_dac*dac_to_fft*fft_to_out
+
+    svr_to_fft = all_paths_recur('svr', 'fft', conns, {})
+    fft_to_dac = all_paths_recur('fft', 'dac', conns, {})
+    dac_to_out = all_paths_recur('dac', 'out', conns, {})
+    option2 = svr_to_fft*fft_to_dac*dac_to_out 
+    return option1 + option2
 
 
 app = AdventDay()
