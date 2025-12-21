de48b5e659992649037ab0fd12367ae6a5bee44c
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-21 20:33:20 +0100

    feat(ec2025): solve day 15 part 3

--- 2025_everybody_codes/quest15.py
+++ 2025_everybody_codes/quest15.py
@@ -1,6 +1,7 @@
 from utils.runner import AdventDay
 from utils.loader import get_file
 from collections import deque
+from dataclasses import dataclass
 
 
 def load(filename):
@@ -34,7 +35,7 @@ def build_graph(data):
     return graph, end
 
 
-def draw(graph, end):
+def draw(graph, start, end):
     flat_x = [n[0] for n in graph]
     flat_y = [n[1] for n in graph]
     max_x = max(flat_x)
@@ -60,7 +61,7 @@ def draw(graph, end):
     for i in range(width):
         for j in range(height):
             point = (min_x+i, min_y+j)
-            if point == (0, 0):
+            if point == start:
                 print('S', end='')
             elif point == end:
                 print('E', end='')
@@ -102,7 +103,7 @@ dirs = [
 ]
 
 
-def shortest_path(points, start, end):
+def shortest_path(points, start, end, path):
     queue = deque([(0, start)])
     visited = set(start)
     while queue:
@@ -112,22 +113,68 @@ def shortest_path(points, start, end):
 
         for dir in dirs:
             neighbor = (node[0]+dir[0], node[1]+dir[1])
+            cost = abs(path.true_x[node[0]] - path.true_x[neighbor[0]])
+            cost += abs(path.true_y[node[1]] - path.true_y[neighbor[1]])
             if neighbor in points:
                 continue
             if neighbor in visited:
                 continue
             visited.add(neighbor)
-            queue.append((distance + 1, neighbor))
+            queue.append((distance + cost, neighbor))
     return -1
+
+
+@dataclass
+class CompressedPath:
+    shrink_x: list
+    shrink_y: list
+    true_x: list
+    true_y: list
+    path: list
+
+    def shrink(self, point):
+        return (self.shrink_x[point[0]], self.shrink_y[point[1]])
+
+    def append(self, point):
+        self.path.append(self.shrink(point))
+
+
+def compress(orig_graph, end):
+    graph = orig_graph + [(0, 0), end]
+    all_xs = set()
+    all_ys = set()
+    for x, y in graph:
+        all_xs.update([x - 1, x, x + 1])
+        all_ys.update([y - 1, y, y + 1])
+    sorted_xs = sorted(list(all_xs))
+    sorted_ys = sorted(list(all_ys))
+
+    shrink_x = {val: i for i, val in enumerate(sorted_xs)}
+    shrink_y = {val: i for i, val in enumerate(sorted_ys)}
+
+    compressed = CompressedPath(
+            shrink_x, shrink_y, sorted_xs, sorted_ys, []
+    )
+    for node in orig_graph:
+        compressed.append(node)
+    return compressed
 
 
 def part1(data):
     graph, end = build_graph(data)
-    w, h, grid = build_grid(graph)
-    return shortest_path(grid, (0, 0), end)
+    compressed = compress(graph, end)
+    w, h, grid = build_grid(compressed.path)
+    end = compressed.shrink(end)
+    start = compressed.shrink((0, 0))
+    # draw(compressed.path, start, end)
+    return shortest_path(grid, start, end, compressed)
 
 
 def part2(data):
+    return part1(data)
+
+
+def part3(data):
     return part1(data)
 
 
