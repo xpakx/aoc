6129ecc864d4b9901bedbd83484b16caa751dfca
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-22 23:16:58 +0100

    feat(ec2025): solve quest 18 part 3

--- 2025_everybody_codes/quest18.py
+++ 2025_everybody_codes/quest18.py
@@ -21,6 +21,7 @@ class Plant:
 def get_all_ints(line):
     return [int(s) for s in re.findall(r'-?\d+', line)]
 
+
 def get_plants(data):
     plants = {}
     have_connections = set()
@@ -49,7 +50,7 @@ def load1(filename):
     return get_plants(data)
 
 
-def load2(filename):
+def load(filename):
     data = get_file(filename, split_by="\n\n")
     plants, last = get_plants(data[:-1])
     instructions = []
@@ -79,6 +80,35 @@ def part1(data, last):
 
 def part2(data, last, instr):
     return sum([traverse(data[last], data, i) for i in instr])
+
+
+def verify(data, instr):
+    positive = [False for _ in instr[0]]
+    negative = [False for _ in instr[0]]
+    for plant in data.values():
+        for branch in plant.branches:
+            if branch.dest is None:
+                assert branch.thickness == 1, "All input branches should have thickness 1"
+                continue
+            if branch.dest > len(instr[0]):
+                assert branch.thickness > 0, f"Branch to {branch.dest} should have positive thickness"
+                continue
+            if branch.thickness < 0:
+                negative[branch.dest-1] = True
+            else:
+                positive[branch.dest-1] = True
+    test = all([a ^ b for a, b in zip(positive, negative)])
+    assert test, "All inputs should contribute only positively or only negatively"
+    return {i+1: (1 if a else 0) for i, a in enumerate(positive)}
+
+
+# TODO: doesn't work for an example
+def part3(data, last, instr):
+    spell = verify(data, instr)
+    max = traverse(data[last], data, spell)
+    ducks = [traverse(data[last], data, i) for i in instr]
+    ducks = [x for x in ducks if x > 0]
+    return sum([max - x for x in ducks])
 
 
 app = AdventDay()
