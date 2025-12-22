2f0ee2a6cbfbc21e6994ad7f539d92c5ca094c8b
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-22 17:35:19 +0100

    feat(ec2025): day 17 part 2

--- 2025_everybody_codes/quest17.py
+++ 2025_everybody_codes/quest17.py
@@ -29,5 +29,35 @@ def part1(data, start):
     return result
 
 
+def make_check_map(data, start):
+    check_map = []
+    for i, row in enumerate(data):
+        check_row = []
+        for j, val in enumerate(row):
+            check = (start[0] - i)**2 + (start[1] - j)**2
+            check_row.append(check)
+        check_map.append(check_row)
+    return check_map
+
+
+def sum_for_step(data, check_map, step):
+    r2 = step**2
+    result = 0
+    for i, row in enumerate(data):
+        for j, val in enumerate(row):
+            if check_map[i][j] <= r2:
+                result += val
+    return result
+
+
+def part2(data, start):
+    check_map = make_check_map(data, start)
+    max_r = min(len(data), len(data[0])) // 2
+    steps = [sum_for_step(data, check_map, i) for i in range(1, max_r+1)]
+    steps = [y-x for x, y in zip([0] + steps, steps)]
+    max_val, max_idx = max((val, idx+1) for idx, val in enumerate(steps))
+    return max_val*max_idx
+
+
 app = AdventDay()
 app.run()
