60c3b65bfa82052d6e7c2b8e22dda4bbe521b95b
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-23 20:18:44 +0100

    feat(ec2025): solved quest 20 part 3

--- 2025_everybody_codes/quest20.py
+++ 2025_everybody_codes/quest20.py
@@ -2,6 +2,7 @@ from utils.loader import get_map
 from utils.runner import AdventDay
 from enum import Enum
 import heapq
+from math import ceil
 
 
 class Tile(Enum):
@@ -81,6 +82,50 @@ def part2(maze):
                 goal = (i, j)
                 maze[i][j] = Tile.trampoline
     return shortest_path(maze, start, goal)
+
+
+# based on barycentric coordinates, but adjusted for 'skewedness'
+def rotate(i, j, maze):
+    j = j-i  # unskewing
+    new_i = (len(maze) - 1) - i - ceil(j/2)
+    new_j = 2*i if j % 2 == 0 else 2 * i + 1
+    new_j = new_j + new_i  # reskewing
+    return (new_i, new_j)
+
+
+def shortest_path_with_rotation(maze, start, goal):
+    heap = [(0, start[0], start[1])]
+    visited = set()
+    while heap:
+        cost, i, j = heapq.heappop(heap)
+        position = (i, j)
+        if position == goal:
+            return cost
+
+        i, j = rotate(i, j, maze)
+        neighbors = get_neighbors(i, j) + [(i, j)]
+
+        for new_pos in neighbors:
+            if new_pos in visited:
+                continue
+            if not trampoline_at(new_pos[0], new_pos[1], maze):
+                continue
+            visited.add(new_pos)
+            heapq.heappush(heap, (cost+1, new_pos[0], new_pos[1]))
+    return -1
+
+
+def part3(maze):
+    goal = (0, 0)
+    start = (0, 0)
+    for i, row in enumerate(maze):
+        for j, cell in enumerate(row):
+            if cell == Tile.start:
+                start = (i, j)
+            if cell == Tile.end:
+                goal = (i, j)
+                maze[i][j] = Tile.trampoline
+    return shortest_path_with_rotation(maze, start, goal)
 
 
 app = AdventDay()
