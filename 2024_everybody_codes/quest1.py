680bc0eb2038b17719d66cc5b4ead86c796e72c2
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-27 21:07:43 +0100

    feat(ec2024): day 1 part 2

--- 2024_everybody_codes/quest1.py
+++ 2024_everybody_codes/quest1.py
@@ -10,5 +10,24 @@ def task1(data):
     return data.count("B") + 3*data.count("C")
 
 
+def cost_of(insect):
+    if insect == 'B':
+        return 1
+    if insect == 'C':
+        return 3
+    if insect == 'D':
+        return 5
+    return 0
+
+
+def task2(data):
+    result = 0
+    for first, second in zip(data[::2], data[1::2]):
+        result += cost_of(first) + cost_of(second)
+        if first != 'x' and second != 'x':
+            result += 2
+    return result
+
+
 app = AdventDay()
 app.run()
