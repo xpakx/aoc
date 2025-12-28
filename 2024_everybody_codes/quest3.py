154eef9a10b9a10cfd4de70a6994a9c199fbf3e5
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-28 22:36:02 +0100

    feat(ec2024): solve day 3 part 3

--- 2024_everybody_codes/quest3.py
+++ 2024_everybody_codes/quest3.py
@@ -20,37 +20,37 @@ def load(filename):
 
 
 dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+dirs_diag = dirs + [(-1, 1), (1, 1), (1, -1), (-1, -1)]
 
 
-def check_neighbors(cells, i, j, nums):
+def check_neighbors(cells, i, j, nums, dirs):
     for dir in dirs:
         i2 = i + dir[0]
         j2 = j + dir[1]
         if i2 < 0 or j2 < 0:
-            continue
+            return False
         if i2 >= len(cells) or j2 >= len(cells[i2]):
-            continue
+            return False
         if cells[i2][j2] != nums:
             return False
     return True
 
 
-def step(data, num):
+def step(data, num, dirs=dirs):
     cells = [row for row in data]
     to_increase = []
     for i, row in enumerate(cells):
         for j, cell in enumerate(row):
-            if check_neighbors(cells, i, j, num):
+            if cell == 0:
+                continue
+            if check_neighbors(cells, i, j, num, dirs):
                 to_increase.append((i, j))
     for i, j in to_increase:
         cells[i][j] += 1
     return len(to_increase)
 
 
-def task1(cells):
-    i = 1
-    while step(cells, i) > 0:
-        i += 1
+def count(cells):
     result = 0
     for row in cells:
         for cell in row:
@@ -58,8 +58,33 @@ def task1(cells):
     return result
 
 
+def draw(cells):
+    for row in cells:
+        for cell in row:
+            if cell == 0:
+                print('.', end='')
+            else:
+                print(cell, end='')
+        print()
+
+
+def task1(cells):
+    i = 1
+    while step(cells, i) > 0:
+        i += 1
+    return count(cells)
+
+
 def task2(cells):
     return task1(cells)
+
+
+def task3(cells):
+    i = 1
+    while step(cells, i, dirs=dirs_diag) > 0:
+        i += 1
+    # draw(cells)
+    return count(cells)
 
 
 app = AdventDay()
