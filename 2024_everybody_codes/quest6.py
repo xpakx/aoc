4248b56a4b9398252e2d84ddf53aa3f2f4e15941
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-29 23:37:28 +0100

    feat(ec2024): solve day 6 part 3

--- 2024_everybody_codes/quest6.py
+++ 2024_everybody_codes/quest6.py
@@ -53,5 +53,25 @@ def task2(graph):
             return fruit_paths[fruit]
 
 
+def task3(graph):
+    nodes = [('RR', 0, set('RR'), 'R')]
+    fruits = {}
+    while nodes:
+        fruit, path_len, visited, path = nodes.pop()
+        if fruit == '@':
+            fruits[path] = path_len
+            continue
+        visited.add(fruit)
+        next = graph.get(fruit)
+        if next is None:
+            continue
+        new = [(n, path_len+1, set(visited), path + n[0]) for n in next if n not in visited]
+        nodes.extend(new)
+    value_counts = Counter(fruits.values())
+    for fruit, val in fruits.items():
+        if value_counts[val] == 1:
+            return fruit
+
+
 app = AdventDay()
 app.run()
