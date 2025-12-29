02aca9ba7f2bdf600311095c5e355a1a2afad79d
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-29 20:43:32 +0100

    feat(ec2025): solve day 5 part 2

--- 2024_everybody_codes/quest5.py
+++ 2024_everybody_codes/quest5.py
@@ -1,5 +1,6 @@
 from utils.loader import get_file
 from utils.runner import AdventDay
+from collections import defaultdict
 
 
 def load(filename):
@@ -12,20 +13,39 @@ def shouted(data):
     return [col[0] for col in data]
 
 
+def cycle(data, i):
+    row = data[i % len(data)]
+    num = row.pop(0)
+    next_row = data[(i+1) % len(data)]
+
+    moves = num - 1
+    if num > len(next_row):
+        moves = 2*len(next_row) - moves
+    next_row.insert(moves, num)
+
+
 def task1(data):
     # print(data)
     for i in range(10):
-        row = data[i % len(data)]
-        num = row.pop(0)
-        next_row = data[(i+1) % len(data)]
-
-        moves = num - 1
-        if num > len(next_row):
-            moves = 2*len(next_row) - moves
-        next_row.insert(moves, num)
+        cycle(data, i)
         # print(f"{i+1}:", shouted(data))
-
     return ''.join(str(x) for x in shouted(data))
+
+
+def task2(data):
+    mem = defaultdict(lambda: 0)
+    i = 0
+    while True:
+        cycle(data, i)
+        a, b, c, d = shouted(data)
+        num = f"{a}{b}{c}{d}"
+        n = mem[num] + 1
+        if n == 2024:
+            print(num, i)
+            return int(num)*(i+1)
+        else:
+            mem[num] = n
+        i += 1
 
 
 app = AdventDay()
