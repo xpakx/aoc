bdd17baa0be3f0e52dbac9b03c9c4cffcbbbe40f
xpakx <55586470+xpakx@users.noreply.github.com>
2025-12-31 20:00:44 +0100

    feat(ec2024): day 8 part 3

--- 2024_everybody_codes/quest8.py
+++ 2024_everybody_codes/quest8.py
@@ -41,5 +41,34 @@ def task2(priests):
     return -blocks*floor
 
 
+def layer3(number, priests, acolytes):
+    return ((number * priests) % acolytes) + acolytes
+
+
+def to_remove(priests, width, height, acolytes):
+    return (priests * width * height) % acolytes
+
+
+def task3(priests):
+    start_blocks = 202400000
+    acolytes = 10
+    curr = 1
+    blocks = start_blocks - curr
+    layer_num = 1
+    cols = [1]
+    while blocks > 0:
+        layer_num += 1
+        curr = layer3(curr, priests, acolytes)
+        blocks_used = 2*layer_num - 1
+        blocks -= blocks_used*curr
+        cols = [curr] + [x+curr for x in cols] + [curr]
+    floor = 2*(layer_num-1) + 1
+    cols = [to_remove(priests, floor, x, acolytes) for x in cols]
+    # print(cols)
+    empty = sum(cols[1:-1])
+
+    return abs(blocks) - empty
+
+
 app = AdventDay()
 app.run()
