912059564a977c643f68a0585b9c8b8519cde756
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-05 22:07:26 +0100

    feat(ec2024): solve day 15 part 2

--- 2024_everybody_codes/quest15.py
+++ 2024_everybody_codes/quest15.py
@@ -36,9 +36,40 @@ def find_path(start, data):
 
 
 def task1(data, start):
-    print(data)
-    print(start)
     return 2*find_path(start, data)
+
+
+def find_path2(start, data, herbs):
+    k = tuple([False]*len(herbs))
+    queue = [(0, start, k)]
+    heapq.heapify(queue)
+    min_costs = {(start, k): 0}
+    while queue:
+        cost, curr, found_herbs = heapq.heappop(queue)
+        if curr == start and all(found_herbs):
+            return cost
+        for neighbor in neighbors(curr):
+            symbol = data[neighbor[0]][neighbor[1]]
+            if symbol == '#':
+                continue
+            if symbol == '~':
+                continue
+            if neighbor[0] < 0:
+                continue
+            val = ord(symbol) - 65
+            new_herbs = tuple([True if i == val else x for i, x in enumerate(found_herbs)])
+            if cost + 1 < min_costs.get((neighbor, new_herbs), float('inf')):
+                min_costs[(neighbor, new_herbs)] = cost + 1
+                heapq.heappush(queue, (cost+1, neighbor, new_herbs))
+
+
+def task2(data, start):
+    herbs = set()
+    for row in data:
+        for cell in row:
+            if cell not in ['#', '~', '.']:
+                herbs.add(cell)
+    return find_path2(start, data, herbs)
 
 
 app = AdventDay()
