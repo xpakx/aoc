5445dfc2086872ba811282d32a8404f06d4a7427
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-05 20:19:04 +0100

    feat(ec2024): solve day 14 part 2

--- 2024_everybody_codes/quest14.py
+++ 2024_everybody_codes/quest14.py
@@ -3,7 +3,12 @@ from utils.runner import AdventDay
 
 
 def load(filename):
-    data = get_file(filename, split_by=',')
+    data = get_file(filename)
+    return [prepare_branch(x) for x in data]
+
+
+def prepare_branch(data):
+    data = data.split(',')
     instr = []
     for entry in data:
         dir = entry[0]
@@ -22,11 +27,43 @@ def load(filename):
 def task1(data):
     height = 0
     max = 0
-    for _, y, _ in data:
+    for _, y, _ in data[0]:
         height += y
         if height > max:
             max = height
     return max
+
+
+def add_segments(segments, start, step):
+    if step[0] != 0:
+        s = step[0]
+        r = range(min(0, s), max(0, s)+1)
+        print(r, step, start)
+        print([(start[0]+x, start[1], start[2]) for x in r])
+        segments.update([(start[0]+x, start[1], start[2]) for x in r])
+    elif step[1] != 0:
+        s = step[1]
+        r = range(min(0, s), max(0, s)+1)
+        print(r, step, start)
+        print([(start[0], start[1]+x, start[2]) for x in r])
+        segments.update([(start[0], start[1]+x, start[2]) for x in r])
+    else:
+        s = step[2]
+        r = range(min(0, s), max(0, s)+1)
+        print(r, step, start)
+        print([(start[0], start[1], start[2]+x) for x in r])
+        segments.update([(start[0], start[1], start[2]+x) for x in r])
+
+
+def task2(data):
+    segments = set()
+    start = (0, 0, 0)
+    for branch in data:
+        seg = start
+        for x, y, z in branch:
+            add_segments(segments, seg, (x, y, z))
+            seg = (seg[0]+x, seg[1]+y, seg[2]+z)
+    return len(segments)-1
 
 
 app = AdventDay()
