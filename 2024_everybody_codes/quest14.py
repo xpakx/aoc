f3b42f9da776eaee1dc0294bcca9d91d31fcfac7
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-05 21:41:36 +0100

    feat(ec2024): solve day 14 part 3

--- 2024_everybody_codes/quest14.py
+++ 2024_everybody_codes/quest14.py
@@ -1,5 +1,6 @@
 from utils.loader import get_file
 from utils.runner import AdventDay
+import heapq
 
 
 def load(filename):
@@ -66,16 +67,92 @@ def task2(data):
     return len(segments)-1
 
 
+def add_to_trunk(trunk, start, step):
+    if step[0] != 0:
+        if step[2] != 0:
+            return
+        p1 = min(start[0], start[0]+step[0])
+        p2 = max(start[0], start[0]+step[0])
+        if p1 <= 0 and p2 >= 0:
+            trunk.add(start[1])
+    elif step[1] != 0:
+        if start[0] != 0 or start[1] != 0:
+            return
+        s = step[1]
+        r = range(min(0, s), max(0, s)+1)
+        trunk.update([start[1]+x for x in r])
+    elif step[2] != 0:
+        if step[0] != 0:
+            return
+        p1 = min(start[2], start[2]+step[2])
+        p2 = max(start[2], start[2]+step[2])
+        if p1 <= 0 and p2 >= 0:
+            trunk.add(start[1])
+
+
+dirs = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, 1), (0, 0, -1)]
+
+
+def neighbors(elem):
+    return [(elem[0]+i, elem[1]+j, elem[2]+k) for i, j, k in dirs]
+
+
+def to_value(symbol):
+    if symbol in ['S', 'E']:
+        return 0
+    return ord(symbol)-48
+
+
+def find_path(start, segments, leaf):
+    queue = [(0, start)]
+    heapq.heapify(queue)
+    min_costs = {start: 0}
+    while queue:
+        cost, curr = heapq.heappop(queue)
+        if curr == leaf:
+            return cost
+        for neighbor in neighbors(curr):
+            if neighbor not in segments:
+                continue
+            if cost + 1 < min_costs.get(neighbor, float('inf')):
+                min_costs[neighbor] = cost + 1
+                heapq.heappush(queue, (cost+1, neighbor))
+
+
 def task3(data):
     start = (0, 0, 0)
     leaves = set()
+    trunk = set()
+    segments = set()
     for branch in data:
         seg = start
         for x, y, z in branch:
+            add_to_trunk(trunk, seg, (x, y, z))
+            add_segments(segments, seg, (x, y, z))
             seg = (seg[0]+x, seg[1]+y, seg[2]+z)
         leaves.add(seg)
     print(leaves)
-    return 0
+    print(trunk)
+
+    min_score = float('inf')
+    scores = []
+    h = -1
+    for height in trunk:
+        score = 0
+        lst = []
+        for leaf in leaves:
+            leaf_score = find_path((0, height, 0), segments, leaf)
+            if leaf_score is None:
+                score = float('inf')
+                continue
+            score += leaf_score
+            lst.append(leaf_score)
+        if score < min_score:
+            min_score = score
+            scores = lst
+            h = height
+    print(scores, h)
+    return min_score
 
 
 app = AdventDay()
