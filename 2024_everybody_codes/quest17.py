3657eb90e3fa9f65219e6e4a1efaf07d9cd648ed
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-07 21:48:26 +0100

    feat(ec2024): solve day 17 part 1 with prim's algorithm

--- 2024_everybody_codes/quest17.py
+++ 2024_everybody_codes/quest17.py
@@ -1,128 +1,53 @@
 from utils.loader import get_file
 from utils.runner import AdventDay
+import heapq
 
 
 def load1(filename):
     data = get_file(filename)
-    start = -1
-    palms = 0
-    for i, row in enumerate(data):
-        if row[0] == '.':
-            start = i
-        for cell in row:
-            if cell == 'P':
-                palms += 1
-    return data, palms, (start, 0)
-
-
-dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-
-def neighbors(elem):
-    return [(elem[0]+i, elem[1]+j) for i, j in dirs]
-
-
-def flood_fill(start, data, goal):
-    filled = set()
-    current = start
-    steps = 0
-    palms = 0
-    while current:
-        steps += 1
-        new_current = []
-        for elem in current:
-            for neighbor in neighbors(elem):
-                if neighbor[0] < 0 or neighbor[1] < 0:
-                    continue
-                if neighbor[0] >= len(data) or neighbor[1] >= len(data[0]):
-                    continue
-                symbol = data[neighbor[0]][neighbor[1]]
-                if symbol == '#':
-                    continue
-                if neighbor in filled:
-                    continue
-                if symbol == 'P':
-                    palms += 1
-                filled.add(neighbor)
-                new_current.append(neighbor)
-        if palms == goal:
-            return steps
-        current = new_current
-    return 0
-
-
-def task1(data, palms, start):
-    return flood_fill([start], data, palms)
-
-
-def load2(filename):
-    data = get_file(filename)
-    start = -1
-    start2 = -1
-    palms = 0
-    for i, row in enumerate(data):
-        if row[0] == '.':
-            start = i
-        if row[len(row)-1] == '.':
-            start2 = i
-        for cell in row:
-            if cell == 'P':
-                palms += 1
-    return data, palms, (start, 0), (start2, len(data[0])-1)
-
-
-def task2(data, palms, start, start2):
-    return flood_fill([start, start2], data, palms)
-
-
-def load3(filename):
-    data = get_file(filename)
-    palms = 0
-    for i, row in enumerate(data):
-        for cell in row:
-            if cell == 'P':
-                palms += 1
-    return data, palms
-
-
-def palm_time(start, data, goal, best):
-    filled = set()
-    current = [start]
-    steps = 0
-    palms = 0
-    dist = 0
-    while current:
-        steps += 1
-        new_current = []
-        for elem in current:
-            for neighbor in neighbors(elem):
-                symbol = data[neighbor[0]][neighbor[1]]
-                if symbol == '#':
-                    continue
-                if neighbor in filled:
-                    continue
-                if symbol == 'P':
-                    palms += 1
-                    dist += steps
-                    if dist >= best:
-                        return None
-                filled.add(neighbor)
-                new_current.append(neighbor)
-        if palms == goal:
-            return dist
-        current = new_current
-    return None
-
-
-def task3(data, palms):
-    best_time = float('inf')
+    stars = []
     for i, row in enumerate(data):
         for j, cell in enumerate(row):
-            if cell == '.':
-                time = palm_time((i, j), data, palms, best_time)
-                if time and time < best_time:
-                    best_time = time
-    return best_time
+            if cell == '*':
+                stars.append((i, j))
+    return stars
+
+
+def manhattan(p1, p2):
+    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+
+
+def spanning_tree(stars, connections):
+    queue = []
+    visited = {star: False for star in stars}
+    result = 0
+    heapq.heappush(queue, (0, stars[0]))
+
+    while queue:
+        weight, node = heapq.heappop(queue)
+
+        if visited[node]:
+            continue
+
+        result += weight
+        visited[node] = True
+
+        for edge in connections[node]:
+            if not visited[edge[1]]:
+                heapq.heappush(queue, (edge[0], edge[1]))
+
+    return result
+
+
+def task1(stars):
+    connections = {star: [] for star in stars}
+    for star in stars:
+        for second in stars:
+            if star == second:
+                continue
+            dist = manhattan(star, second)
+            connections[star].append((dist, second))
+    return spanning_tree(stars, connections) + len(stars)
 
 
 app = AdventDay()
