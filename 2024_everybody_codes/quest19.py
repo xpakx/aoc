7a8e01428664668d3cd185f32504269baab06d35
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-08 20:53:28 +0100

    feat(ec2024): solve day 19 part 3

--- 2024_everybody_codes/quest19.py
+++ 2024_everybody_codes/quest19.py
@@ -51,8 +51,6 @@ def get_msg(msg):
 
 
 def task1(instr, msg):
-    print_msg(msg)
-    print()
     dir_i = 0
     for i in range(1, len(msg)-1):
         for j in range(1, len(msg[i])-1):
@@ -62,8 +60,6 @@ def task1(instr, msg):
             else:
                 rotate_right(msg, (i, j))
             dir_i = (dir_i + 1) % len(instr)
-            print_msg(msg)
-            print()
     print(instr)
     print_msg(msg)
     return get_msg(msg)
@@ -83,6 +79,57 @@ def task2(instr, msg):
     print(instr)
     print_msg(msg)
     return get_msg(msg)
+
+
+offsets = [(-1, -1), (-1, 0), (-1, 1), (0, 1),
+           (1, 1), (1, 0), (1, -1), (0, -1)]
+
+
+def find_cycles(grid, sequence):
+    height, width = len(grid), len(grid[0])
+    tgrid = [[(y, x) for x in range(width)] for y in range(height)]
+    i = 0
+    for y in range(1, height-1):
+        for x in range(1, width-1):
+            dj = sequence[i % len(sequence)]
+            vals = [tgrid[y+dy][x+dx] for (dy, dx) in offsets]
+            for j, val in enumerate(vals):
+                dy, dx = offsets[(j+dj) % 8]
+                tgrid[y+dy][x+dx] = val
+            i += 1
+
+    transition = {}
+    for y in range(height):
+        for x in range(width):
+            transition[tgrid[y][x]] = (y, x)
+
+    cycles = []
+    seen = set()
+    for sy in range(height):
+        for sx in range(width):
+            if (sy, sx) in seen:
+                continue
+            cycle = []
+            y, x = sy, sx
+            while (y, x) not in seen:
+                cycle.append((y, x))
+                seen.add((y, x))
+                y, x = transition[y, x]
+            cycles.append(cycle)
+    return cycles
+
+
+def task3(instr, msg):
+    rounds = 1048576000
+    cycles = find_cycles(msg, instr)
+
+    new_msg = [[None]*len(msg[0]) for _ in range(len(msg))]
+    for cycle in cycles:
+        for i, (sy, sx) in enumerate(cycle):
+            dy, dx = cycle[(i+rounds) % len(cycle)]
+            new_msg[dy][dx] = msg[sy][sx]
+
+    return get_msg(new_msg)
 
 
 app = AdventDay()
