7f4edf0da49cc5c87d9a69b5479abc1f096595fd
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-08 20:16:55 +0100

    feat(ec2024): solve day 19 part 1

---
+++ 2024_everybody_codes/quest19.py
@@ -0,0 +1,71 @@
+from utils.loader import get_file_instr
+from utils.runner import AdventDay
+
+
+def load(filename):
+    instr, msg = get_file_instr(filename, split_second_by='\n')
+    instr = [1 if c == 'R' else -1 for c in instr[0]]
+    msg = [list(s) for s in msg]
+    return instr, msg
+
+
+def rotate_right(msg, point):
+    i, j = point
+    temp = msg[i-1][j-1]
+    msg[i-1][j-1] = msg[i][j-1]
+    msg[i][j-1] = msg[i+1][j-1]
+    msg[i+1][j-1] = msg[i+1][j]
+    msg[i+1][j] = msg[i+1][j+1]
+    msg[i+1][j+1] = msg[i][j+1]
+    msg[i][j+1] = msg[i-1][j+1]
+    msg[i-1][j+1] = msg[i-1][j]
+    msg[i-1][j] = temp
+
+
+def rotate_left(msg, point):
+    i, j = point
+    temp = msg[i-1][j-1]
+    msg[i-1][j-1] = msg[i-1][j]
+    msg[i-1][j] = msg[i-1][j+1]
+    msg[i-1][j+1] = msg[i][j+1]
+    msg[i][j+1] = msg[i+1][j+1]
+    msg[i+1][j+1] = msg[i+1][j]
+    msg[i+1][j] = msg[i+1][j-1]
+    msg[i+1][j-1] = msg[i][j-1]
+    msg[i][j-1] = temp
+
+
+def print_msg(msg):
+    for row in msg:
+        for cell in row:
+            print(cell, end='')
+        print()
+
+
+def get_msg(msg):
+    for row in msg:
+        if row[0] == '>':
+            return ''.join(row[1:-1])
+
+
+def task1(instr, msg):
+    print_msg(msg)
+    print()
+    dir_i = 0
+    for i in range(1, len(msg)-1):
+        for j in range(1, len(msg[i])-1):
+            dir = instr[dir_i]
+            if dir < 0:
+                rotate_left(msg, (i, j))
+            else:
+                rotate_right(msg, (i, j))
+            dir_i = (dir_i + 1) % len(instr)
+            print_msg(msg)
+            print()
+    print(instr)
+    print_msg(msg)
+    return get_msg(msg)
+
+
+app = AdventDay()
+app.run()
