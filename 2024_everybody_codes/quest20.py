08d5d5adba59aff5156e8c6dc7df8be5110f2b3a
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-09 19:10:52 +0100

    feat(ec2024): collect best partial paths for q10p3

--- 2024_everybody_codes/quest20.py
+++ 2024_everybody_codes/quest20.py
@@ -58,7 +58,7 @@ def part1(data, start):
 
 
 def find_path2(start, data):
-    # steps, checkpoints, alt, pos, prev
+    # steps, alt, checkpoints, pos, prev
     queue = deque([(0, 10000, 0, start, None)])
     best_states = {}
     while queue:
@@ -112,6 +112,59 @@ def find_path2(start, data):
 
 def part2(data, start):
     return find_path2(start, data)
+
+
+def find_path_segment(start, data):
+    # alt, steps, pos, prev
+    queue = deque([(1000, start, None)])
+    max_alt = 0
+    dest = start
+    best_states = {}
+    while queue:
+        altitude, curr, prev = queue.popleft()
+        if curr[0] == len(data)-1:
+            if altitude > max_alt:
+                max_alt = altitude
+                dest = curr
+            continue
+        for neighbor in neighbors(curr):
+            if neighbor[0] < 0 or neighbor[1] < 0:
+                continue
+            if neighbor[0] >= len(data) or neighbor[1] >= len(data[0]):
+                continue
+            if neighbor == prev:
+                continue
+            symbol = data[neighbor[0]][neighbor[1]]
+            if symbol == '#':
+                continue
+            new_alt = altitude
+            if symbol == '-':
+                new_alt -= 2
+            elif symbol == '+':
+                new_alt += 1
+            else:
+                new_alt -= 1
+            if new_alt < 0:
+                continue
+
+            state_key = (neighbor, curr)
+            if state_key in best_states:
+                if best_states[state_key] >= new_alt:
+                    continue
+            best_states[state_key] = new_alt
+            queue.append((new_alt, neighbor, curr))
+    return max_alt, dest
+
+
+def part3(data, start):
+    m = {}
+    for i in range(len(data[0])-1):
+        if i == 0:
+            continue
+        start = (0, i)
+        alt, (_, y) = find_path_segment(start, data)
+        m[start] = ((0, y), 1000-alt+1)
+    print(m)
 
 
 app = AdventDay()
