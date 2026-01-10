fb557911f273e1eef43b5b137c7c7888f56c8ddb
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-10 22:26:59 +0100

    feat(ecs1): solve day 1 part 1

--- 2025_everybody_codes_story_1/quest1.py
+++ 2025_everybody_codes_story_1/quest1.py
@@ -1,13 +1,42 @@
 from utils.loader import get_file
 from utils.runner import AdventDay
+from utils.parser import parse
+from dataclasses import dataclass
 
 
-def load(filename):
-    return get_file(filename)
+@dataclass
+class Note:
+    a: int
+    b: int
+    c: int
+    x: int
+    y: int
+    z: int
+    m: int
+
+
+def load(filename) -> list[Note]:
+    return parse(Note, "A={a} B={b} C={c} X={x} Y={y} Z={z} M={m}", get_file(filename))
+
+
+def eni(n: int, exp: int, mod: int) -> int:
+    score = 1
+    result = ""
+    for _ in range(exp):
+        score = (score * n) % mod
+        result = str(score) + result
+    return int(result)
+
+
+def eni_for_note(note: Note) -> int:
+    result = eni(note.a, note.x, note.m) 
+    result += eni(note.b, note.y, note.m)
+    result += eni(note.c, note.z, note.m)
+    return result
 
 
 def task1(data):
-    print(data)
+    return max([eni_for_note(x) for x in data])
 
 
 app = AdventDay()
