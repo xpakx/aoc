540b671d94fb1e24147a5640be04c52559dc425e
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-13 17:58:33 +0100

    feat(ecs2): solve day 2 part 2

--- 2025_everybody_codes_story_2/quest2.py
+++ 2025_everybody_codes_story_2/quest2.py
@@ -19,6 +19,23 @@ def task1(data):
     return turn
 
 
+def task2(data):
+    balloons = data * 100
+    balloons = list(balloons)
+    balls = 'RGB'
+    turn = 0
+    while balloons:
+        ball_idx = turn % len(balls)
+        ball = balls[ball_idx]
+        other = None
+        if len(balloons) % 2 == 0:
+            other = len(balloons) // 2
+        if ball == balloons[0] and other is not None:
+            balloons.pop(other)
+        balloons.pop(0)
+        turn += 1
+    return turn
+
 
 app = AdventDay()
 app.run()
