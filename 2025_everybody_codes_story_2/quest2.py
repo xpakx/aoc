57129436b6f4d8c1a904eeb470eb61c71616fcc3
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-13 18:17:20 +0100

    feat(ecs2): solve day 2 part 3

--- 2025_everybody_codes_story_2/quest2.py
+++ 2025_everybody_codes_story_2/quest2.py
@@ -1,5 +1,6 @@
 from utils.loader import get_file
 from utils.runner import AdventDay
+from collections import deque
 
 
 def load(filename) -> tuple[list[str], list[list[int]]]:
@@ -19,22 +20,78 @@ def task1(data):
     return turn
 
 
-def task2(data):
-    balloons = data * 100
-    balloons = list(balloons)
+class MiddleList:
+    def __init__(self, iterable=None):
+        self.left = deque()
+        self.right = deque()
+        if iterable:
+            for item in iterable:
+                self.append(item)
+
+    def _balance(self):
+        if len(self.right) > len(self.left):
+            self.left.append(self.right.popleft())
+        elif len(self.left) > len(self.right) + 1:
+            self.right.appendleft(self.left.pop())
+
+    def append(self, item):
+        self.right.append(item)
+        self._balance()
+
+    def pop_first(self):
+        if not self.left:
+            return None
+
+        val = self.left.popleft()
+        self._balance()
+        return val
+
+    def peek_first(self):
+        return self.left[0]
+
+    def pop_middle(self):
+        if not self.left:
+            return None
+
+        if len(self.left) == len(self.right):
+            val = self.right.popleft()
+        else:
+            val = self.left.pop()
+
+        self._balance()
+        return val
+
+    def __len__(self):
+        return len(self.left) + len(self.right)
+
+    def __repr__(self):
+        return f"[{list(self.left)} | {list(self.right)}]"
+
+
+def solve_circle(data, repeats):
+    balloons = data * repeats
+    balloons = MiddleList(balloons)
     balls = 'RGB'
     turn = 0
     while balloons:
         ball_idx = turn % len(balls)
         ball = balls[ball_idx]
-        other = None
+        other = False
         if len(balloons) % 2 == 0:
-            other = len(balloons) // 2
-        if ball == balloons[0] and other is not None:
-            balloons.pop(other)
-        balloons.pop(0)
+            other = True
+        if ball == balloons.peek_first() and other:
+            balloons.pop_middle()
+        balloons.pop_first()
         turn += 1
     return turn
+
+
+def task2(data):
+    return solve_circle(data, 100)
+
+
+def task3(data):
+    return solve_circle(data, 100000)
 
 
 app = AdventDay()
