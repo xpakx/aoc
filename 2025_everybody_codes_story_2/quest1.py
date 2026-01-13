57794fe74e13f9ebea2b4029ea47310064f1d950
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-13 16:43:34 +0100

    feat(ecs1): find minimum coins in day 1

--- 2025_everybody_codes_story_2/quest1.py
+++ 2025_everybody_codes_story_2/quest1.py
@@ -1,5 +1,6 @@
 from utils.loader import get_file_instr
 from utils.runner import AdventDay
+import heapq
 
 
 def load(filename) -> tuple[list[str], list[list[int]]]:
@@ -40,8 +41,6 @@ def task1(board: list[str], instructions: list[list[int]]):
     coins = 0
     for i, instr in enumerate(instructions):
         coins += drop(i, board, instr)
-    print(board)
-    print(instructions)
     return coins
 
 
@@ -49,18 +48,59 @@ def task2(board: list[str], instructions: list[list[int]]):
     coins = 0
     for instr in instructions:
         max_coins = 0
-        slot = 0
         for i in range((len(board[0])+1)//2):
             curr_coins = drop(i, board, instr)
             # print("   ", i+1, curr_coins)
             if curr_coins > max_coins:
                 max_coins = curr_coins
-                slot = i+1
         coins += max_coins
-        print(slot, max_coins)
-    print(board)
-    print(instructions)
     return coins
+
+
+def finished(values: dict[tuple[int, int], int], state: tuple[int]):
+    seen = 0
+    for i in range(6):
+        if state[i] == 0:
+            return False
+        mask = 1 << state[i]-1
+        if seen & mask != 0:
+            return False
+        seen |= mask
+    return True
+
+
+def find_min(values: dict[tuple[int, int], int], slots_len: int):
+    state = tuple([0] * 6)
+    queue = [(0, state)]
+    visited = set()
+    visited.add(state)
+    while queue:
+        coins, state = heapq.heappop(queue)
+        if finished(values, state):
+            return coins
+        for i in range(6):
+            next_state = tuple([x if j != i else x+1 for j, x in enumerate(state)])
+            if next_state[i] > slots_len:
+                continue
+            old = values.get((state[i], i), 0)
+            new = values.get((next_state[i], i), 0)
+            next_coins = coins - old + new
+            if next_state in visited:
+                continue
+            visited.add(next_state)
+            heapq.heappush(queue, (next_coins, next_state))
+    assert False
+
+
+def task3(board: list[str], instructions: list[list[int]]):
+    values = {}  # (insert_slot, instruction) -> coins
+    slots = (len(board[0])+1) // 2
+    for slot in range(slots):
+        for instr_id, instr in enumerate(instructions):
+            values[(slot+1, instr_id)] = drop(slot, board, instr)
+    print(slots)
+    print(values)
+    return find_min(values, slots)
 
 
 app = AdventDay()
