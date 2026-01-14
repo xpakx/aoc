2d6fa7ccde65c48dc9a34b3b6a1e715bbb690669
xpakx <55586470+xpakx@users.noreply.github.com>
2026-01-14 22:42:55 +0100

    feat(ecs2): solve day 3 part 2

--- 2025_everybody_codes_story_2/quest3.py
+++ 2025_everybody_codes_story_2/quest3.py
@@ -1,4 +1,4 @@
-from utils.loader import get_file
+from utils.loader import get_file, get_file_instr
 from utils.parser import parse
 from utils.runner import AdventDay
 from dataclasses import dataclass
@@ -31,7 +31,7 @@ class Dice:
         return self.pulse
 
 
-def load(filename) -> list[Dice]:
+def load1(filename) -> list[Dice]:
     data = get_file(filename)
     return parse(Dice, "{id}: faces=[{faces}] seed={seed}", data,
                  list_separator=',')
@@ -52,6 +52,56 @@ def task1(dices: list[Dice]) -> int:
             dice.update_pulse(roll_number)
     print(dices)
     return roll_number
+
+
+def load(filename) -> tuple[list[Dice], list[int]]:
+    data, instr = get_file_instr(filename, split_first_by='\n')
+    dices = parse(Dice, "{id}: faces=[{faces}] seed={seed}", data,
+                  list_separator=',')
+    return dices, [int(x) for x in list(instr[0])]
+
+
+class Player:
+    def __init__(self, dice: Dice, track: list[int]):
+        self.dice = dice
+        dice.reset()
+        self.position = 0
+        self.finished = False
+        self.finished_at = 0
+        self.track = track
+
+    def move(self, roll_number: int) -> bool:
+        current = self.track[self.position]
+        result = self.dice.roll(roll_number)
+        self.dice.update_pulse(roll_number)
+        if result == current:
+            self.position += 1
+            self.finished = self.position >= len(self.track)
+        if self.finished:
+            self.finished_at = roll_number
+
+        return self.finished
+
+
+def task2(dices: list[Dice], racetrack: list[int]) -> int:
+    print(dices)
+    print(racetrack)
+    players = [Player(dice, racetrack) for dice in dices]
+    roll_number = 0
+    while True:
+        roll_number += 1
+        moved = False
+        for player in players:
+            if player.finished:
+                continue
+            moved = True
+            player.move(roll_number)
+        if not moved:
+            break
+    result = [(p.finished_at, p.dice.id) for p in players]
+    result.sort()
+    result = [str(p[1]) for p in result]
+    return ','.join(result)
 
 
 app = AdventDay()
